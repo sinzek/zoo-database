96321963fdb370a0d1bbc1fd51a05e28d50e85bd
rsivaku <117036297+rsivaku@users.noreply.github.com>
2025-10-17 19:54:31 -0500

    Delete zoo-database-main directory

--- zoo-database-main/main/api/src/routes/index.js
+++
@@ -1,5 +0,0 @@
-// this file re-exports all route registration functions
-// so they can be imported from a single location
-// (just makes imports cleaner in index.js)
-
-export * from './auth.js';
