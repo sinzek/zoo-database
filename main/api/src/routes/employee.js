186879d7feacc8cd4d7d465140669cf8adf866df
Chase Brock <chase.p.brock@gmail.com>
2025-10-23 19:36:12 -0500

    fix: getAll and deleteOne

--- main/api/src/routes/employee.js
+++ main/api/src/routes/employee.js
@@ -1,14 +1,32 @@
-import employeeController from "../controllers/employeeController.js";
-import { withAccessLevel } from "../utils/auth-utils.js";
+import employeeController from '../controllers/employeeController.js';
+import { withAccessLevel } from '../utils/auth-utils.js';
 
 export function registerEmployeeRoutes(app) {
-  app.post("/api/employee/create", withAccessLevel('executive', employeeController.createOne));
-  app.post("/api/employee/get-one-by-id", withAccessLevel('manager', employeeController.getOneById));
-  app.post(
-    "/api/employee/get-n-by-business",
-   withAccessLevel('manager', employeeController.getNByBusiness)
-  );
-  app.put("/api/employee/update", withAccessLevel('executive', employeeController.updateOne));
-  app.post("/api/employee/get-n-by-animal", withAccessLevel('zookeeper', employeeController.getNByAnimal));
-  app.post("/api/employee/get-n-by-business-and-access-level", withAccessLevel('manager', employeeController.getNByBusinessAndAccessLevel));
+	app.post(
+		'/api/employee/create',
+		withAccessLevel('executive', employeeController.createOne)
+	);
+	app.post(
+		'/api/employee/get-one-by-id',
+		withAccessLevel('manager', employeeController.getOneById)
+	);
+	app.post(
+		'/api/employee/get-n-by-business',
+		withAccessLevel('manager', employeeController.getNByBusiness)
+	);
+	app.put(
+		'/api/employee/update',
+		withAccessLevel('executive', employeeController.updateOne)
+	);
+	app.post(
+		'/api/employee/get-n-by-animal',
+		withAccessLevel('zookeeper', employeeController.getNByAnimal)
+	);
+	app.post(
+		'/api/employee/get-n-by-business-and-access-level',
+		withAccessLevel(
+			'manager',
+			employeeController.getNByBusinessAndAccessLevel
+		)
+	);
 }
