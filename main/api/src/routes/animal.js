e27350d9eb4758e4389fb2577180cd2ff7943fe9
TheRealDuy <tranhoangduy463@gmail.com>
2025-10-26 16:56:37 -0600

    Update animal.js

--- main/api/src/routes/animal.js
+++ main/api/src/routes/animal.js
@@ -1,5 +1,6 @@
 import { withAccessLevel } from "../utils/auth-utils"
 import animalController from "../controllers/animalController.js"
+import animalReportController from "../controllers/animalReportController.js";
 
 export function registerAnimalRoutes(app) {
     app.post('/api/animal/create', withAccessLevel('zookeeper', animalController.createOne));
@@ -7,4 +8,5 @@ export function registerAnimalRoutes(app) {
     app.post('/api/animal/get-one-by-id', animalController.getOneById);
     app.post('/api/animal/get-n-by-habitat', animalController.getNByHabitat);
     app.post('/api/animal/get-n-by-handler', animalController.getNByHandler);
-}
+    app.post('/api/animal/report', withAccessLevel('zookeeper', animalReportController.getAnimalReport));
+}   
